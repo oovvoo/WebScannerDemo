6ea8c6ada76a386498c6cfb4f62f1a1805697297
oovvo <oovvoo_@naver.com>
2022-03-08 23:29:17 +0900

    Initial commit

---
+++ src/test/java/com/secretcode/webscannerdemo/WebScannerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretcode.webscannerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebScannerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
